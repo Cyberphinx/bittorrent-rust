1086f28538c454a8fa0d8b8cbec7f17b5d287803
Cyberphinx <88593809+Cyberphinx@users.noreply.github.com>
2024-07-26 00:35:54 +0100

    feat: decode bencoded integers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use core::panic;
 use std::env;
 
+use serde_json::Value;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
@@ -17,14 +19,23 @@ fn main() {
 }
 
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+    let first_char = encoded_value.chars().next().unwrap();
     // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_ascii_digit() {
+    if first_char.is_ascii_digit() {
         // Example: "5:hello" -> "hello"
         let colon_index = encoded_value.find(':').unwrap();
         let number_string = &encoded_value[..colon_index];
         let number = number_string.parse::<i64>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
         serde_json::Value::String(string.to_string())
+    } else if first_char == 'i' {
+        // Example: "i45e" -> 45
+        let value: String = encoded_value
+            .chars()
+            .filter(|c| c.is_ascii_digit())
+            .collect();
+
+        Value::String(value)
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
