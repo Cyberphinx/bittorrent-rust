a6ad1ae1dd8f6538607bf992070da272099e19fa
Cyberphinx <88593809+Cyberphinx@users.noreply.github.com>
2024-07-27 12:27:23 +0100

    refactor: reduce code repetition

--- src/decode.rs
+++ src/decode.rs
@@ -19,30 +19,13 @@ use serde_json::{json, Value};
 ///
 /// 4. Dictionaries: Encoded as d<contents>e. For example, a dictionary with keys "bar" and "foo" and values "spam" and 42, respectively, is encoded as d3:bar4:spam3:fooi42ee
 ///
-pub fn decode_bencoded_value(encoded_value: &str) -> Result<serde_json::Value> {
+pub fn decode_bencoded_value(encoded_value: &str) -> Result<Value> {
     let first_char = encoded_value.chars().next().unwrap();
 
     if first_char.is_ascii_digit() {
         // Example: "5:hello" -> "hello"
-        if let Some((len, value)) = encoded_value.split_once(':') {
-            // Parse the length part to an integer
-            if let Ok(expected_length) = len.parse::<usize>() {
-                // Check if the length of the value matches the expected length
-                if value.len() == expected_length {
-                    Ok(Value::String(value.to_string()))
-                } else {
-                    Err(eyre::eyre!(
-                        "Length of the value '{}' does not match the expected length {}",
-                        value,
-                        expected_length
-                    ))
-                }
-            } else {
-                Err(eyre::eyre!(
-                    "Failed to parse the length part '{}' as an integer",
-                    len
-                ))
-            }
+        if let Some((length, value)) = encoded_value.split_once(':') {
+            Decoder::decode_string(length, value)
         } else {
             Err(eyre!(
                 "Encoded value doesn't contain valid values: {}",
@@ -51,23 +34,16 @@ pub fn decode_bencoded_value(encoded_value: &str) -> Result<serde_json::Value> {
         }
     } else if first_char == 'i' {
         // Example: "i45e" -> 45
-        let value = encoded_value
-            .chars()
-            .filter(|c| c.is_ascii_digit())
-            .collect::<String>();
-        Ok(Value::String(value))
+        Decoder::decode_integer(encoded_value)
     } else if first_char == 'l' {
         // Example: "l5:helloi52ee"
-        if let Some(value_after_colon) = encoded_value.split(':').nth(1) {
-            let mut splitted_value = value_after_colon.split('i');
+        if let Some((length, value)) = encoded_value.split_once(':') {
+            let mut splitted_value = value.split('i');
             let string_value = splitted_value.next();
             let number_value = splitted_value.next();
             if let (Some(string_value), Some(number_value)) = (string_value, number_value) {
-                let s_val = string_value.to_string();
-                let n_val = number_value
-                    .chars()
-                    .filter(|c| c.is_ascii_digit())
-                    .collect::<String>();
+                let s_val = Decoder::decode_string(length, string_value)?;
+                let n_val = Decoder::decode_integer(number_value)?;
                 Ok(json!((s_val, n_val)))
             } else {
                 Err(eyre!(
@@ -76,10 +52,48 @@ pub fn decode_bencoded_value(encoded_value: &str) -> Result<serde_json::Value> {
                 ))
             }
         } else {
-            Err(eyre!("array does not seem to be a list"))
+            Err(eyre!("Array does not seem to be a list"))
         }
     } else {
         Err(eyre!("Unhandled encoded value: {}", encoded_value))
+    }
+}
+
+struct Decoder;
+
+impl Decoder {
+    fn decode_string(length: &str, value: &str) -> Result<Value> {
+        // Parse the length part to an integer, filtering out non-numeric values
+        if let Ok(expected_length) = length
+            .chars()
+            .filter(|c| c.is_ascii_digit())
+            .collect::<String>()
+            .parse::<usize>()
+        {
+            // Check if the length of the value matches the expected length
+            if value.len() == expected_length {
+                Ok(Value::String(value.to_string()))
+            } else {
+                Err(eyre::eyre!(
+                    "Length of the value '{}' does not match the expected length {}",
+                    value,
+                    expected_length
+                ))
+            }
+        } else {
+            Err(eyre::eyre!(
+                "Failed to parse the length part '{}' as an integer",
+                length
+            ))
+        }
+    }
+
+    fn decode_integer(value: &str) -> Result<Value> {
+        let value = value
+            .chars()
+            .filter(|c| c.is_ascii_digit())
+            .collect::<String>();
+        Ok(Value::String(value))
     }
 }
 
